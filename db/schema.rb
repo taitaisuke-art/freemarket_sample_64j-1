f899c1688bb1f747cfe31074cd416ee0cbb106c4
taitaisuke-art <56817737+taitaisuke-art@users.noreply.github.com>
2019-12-04 15:32:09 +0900

    readme_items

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191204041559) do
+ActiveRecord::Schema.define(version: 20191204042158) do
 
   create_table "addresses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "post_cord",                 null: false
@@ -23,6 +23,50 @@ ActiveRecord::Schema.define(version: 20191204041559) do
     t.datetime "updated_at",                null: false
   end
 
+  create_table "brands", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "brand_name", null: false
+  end
+
+  create_table "brands_groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer "brand_id", null: false
+    t.integer "group_id", null: false
+  end
+
+  create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "category_name", null: false
+  end
+
+  create_table "commnets", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.text    "comment", limit: 65535, null: false
+    t.integer "item_id",               null: false
+    t.integer "user_id",               null: false
+  end
+
+  create_table "groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "brand_type", null: false
+  end
+
+  create_table "item_images", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.text    "image",   limit: 65535
+    t.integer "item_id",               null: false
+  end
+
+  create_table "items", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name",                          null: false
+    t.string   "size"
+    t.string   "conndition",                    null: false
+    t.string   "shipping_method",               null: false
+    t.string   "shipping_days",                 null: false
+    t.string   "shipping_area",                 null: false
+    t.integer  "shipping_price",                null: false
+    t.integer  "item_price",                    null: false
+    t.text     "item_text",       limit: 65535, null: false
+    t.integer  "user_id",                       null: false
+    t.integer  "brand_id",                      null: false
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
+  end
+
   create_table "likes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "item_id",    null: false
     t.integer  "user_id",    null: false
@@ -30,19 +74,25 @@ ActiveRecord::Schema.define(version: 20191204041559) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.text    "message",   limit: 65535, null: false
+    t.integer "item_id",                 null: false
+    t.integer "buyer_id",                null: false
+    t.integer "seller_id",               null: false
+  end
+
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "email",                                default: "", null: false
-    t.string   "encrypted_password",                   default: "", null: false
     t.string   "nickname",                                          null: false
-    t.text     "text",                   limit: 65535,              null: false
     t.string   "first_name",                                        null: false
     t.string   "last_name",                                         null: false
     t.string   "first_name_kana",                                   null: false
     t.string   "last_name_kana",                                    null: false
-    t.integer  "telephone",                                         null: false
     t.text     "icon",                   limit: 65535
     t.string   "birthday",                                          null: false
+    t.text     "text",                   limit: 65535,              null: false
     t.integer  "telephone",                                         null: false
+    t.string   "email",                                default: "", null: false
+    t.string   "encrypted_password",                   default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
