8477f6b2144ff8c82e746a3ffc682156bc4315ba
tikayk <tikayk@gmail.com>
2019-12-09 16:19:45 +0900

    アソシエーション（profile）

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one :address, dependent: :destroy
+  has_one :profile, dependent: :destroy
   has_many :messages, dependent: :destroy
   has_many :items, dependent: :destroy
   has_many :comments, dependent: :destroy
