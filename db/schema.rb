03d7ca679f09b74583152faf94aae13d37e79154
tsunemoto1098 <t-kosuke666@au.com>
2019-12-09 17:13:25 +0900

    slick崩れ　編集

--- db/schema.rb
+++ db/schema.rb
@@ -98,22 +98,22 @@ ActiveRecord::Schema.define(version: 20191209020247) do
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "nickname",                            null: false
-    t.string   "first_name",                          null: false
-    t.string   "last_name",                           null: false
-    t.string   "first_name_kana",                     null: false
-    t.string   "last_name_kana",                      null: false
-    t.string   "birthyear",                           null: false
-    t.string   "birthmonth",                          null: false
-    t.string   "birthday",                            null: false
-    t.string   "telephone",                           null: false
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "nickname",                                          null: false
+    t.string   "first_name",                                        null: false
+    t.string   "last_name",                                         null: false
+    t.string   "first_name_kana",                                   null: false
+    t.string   "last_name_kana",                                    null: false
+    t.text     "icon",                   limit: 65535
+    t.string   "birthday",                                          null: false
+    t.text     "text",                   limit: 65535,              null: false
+    t.integer  "telephone",                                         null: false
+    t.string   "email",                                default: "", null: false
+    t.string   "encrypted_password",                   default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                                        null: false
+    t.datetime "updated_at",                                        null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
