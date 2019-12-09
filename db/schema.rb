e5cac488a4d272069e96077d0a2ca21846a0515d
taitaisuke-art <56817737+taitaisuke-art@users.noreply.github.com>
2019-12-09 17:09:23 +0900

    flontviews2

--- db/schema.rb
+++ db/schema.rb
@@ -32,11 +32,6 @@ ActiveRecord::Schema.define(version: 20191209020247) do
     t.string "brand_name", null: false
   end
 
-  create_table "brands_groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.integer "brand_id", null: false
-    t.integer "group_id", null: false
-  end
-
   create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "category_name", null: false
   end
@@ -48,10 +43,6 @@ ActiveRecord::Schema.define(version: 20191209020247) do
   end
 
   create_table "genres", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string "brand_type", null: false
-  end
-
-  create_table "groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "brand_type", null: false
   end
 
@@ -98,22 +89,22 @@ ActiveRecord::Schema.define(version: 20191209020247) do
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "nickname",                            null: false
-    t.string   "first_name",                          null: false
-    t.string   "last_name",                           null: false
-    t.string   "first_name_kana",                     null: false
-    t.string   "last_name_kana",                      null: false
-    t.string   "birthyear",                           null: false
-    t.string   "birthmonth",                          null: false
-    t.string   "birthday",                            null: false
-    t.string   "telephone",                           null: false
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "nickname",                                          null: false
+    t.string   "first_name",                                        null: false
+    t.string   "last_name",                                         null: false
+    t.string   "first_name_kana",                                   null: false
+    t.string   "last_name_kana",                                    null: false
+    t.text     "icon",                   limit: 65535
+    t.string   "birthday",                                          null: false
+    t.text     "text",                   limit: 65535,              null: false
+    t.integer  "telephone",                                         null: false
+    t.string   "email",                                default: "", null: false
+    t.string   "encrypted_password",                   default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                                        null: false
+    t.datetime "updated_at",                                        null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
