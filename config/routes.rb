0adfd0da367109cc4593e7497ebdbdf09dba6da0
tsunemoto1098 <t-kosuke666@au.com>
2019-12-10 16:31:06 +0900

    profileページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
-  resources :users, only: [:index, :edit, :update]
-  resources :items, only: [:index,:new, :create] 
+  resources :users, only: [:index, :edit, :update] do
+  end
+  resources :profiles
+  resources :items, only: [:index,:new, :create]
 
   resources :signup do
     collection do
