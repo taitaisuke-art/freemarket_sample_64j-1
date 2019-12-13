ca813890b8ff8ff4ccc8dab80564544491931e2c
pekepon476 <57280168+pekepon476@users.noreply.github.com>
2019-12-13 17:05:02 +0900

    ブランチ

--- config/routes.rb
+++ config/routes.rb
@@ -13,10 +13,7 @@ Rails.application.routes.draw do
   resources :signup do
     collection do
       get 'step0'
-      get 'step1'
       get 'step2'
-      get 'step3'
-      get 'step4' # ここで、入力の全てが終了する
       get 'done' # 登録完了後のページ
     end
   end
