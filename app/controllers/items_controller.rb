dbcdf176787e5e25e6312e2c01231365f18be8e9
tikayk <tikayk@gmail.com>
2019-12-17 13:31:58 +0900

    プルリク用

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,26 +1,26 @@
-class ItemsController < ApplicationController
+# class ItemsController < ApplicationController
 
-  def new
-    @item = Item.new
-    @item.item_images.build
-  end
+#   def new
+#     @item = Item.new
+#     @item.item_images.build
+#   end
 
-  def create
-    @item = Item.new(item_params)
-    if @item.save
-      params[:item_images][:image].each do |image|
-        @item.item_images.create(image: image)
-      end
-      redirect_to root_path
-    else
-      redirect_to users_path
-    end
-  end
+#   def create
+#     @item = Item.new(item_params)
+#     if @item.save
+#       params[:item_images][:image].each do |image|
+#         @item.item_images.create(image: image)
+#       end
+#       redirect_to root_path
+#     else
+#       redirect_to users_path
+#     end
+#   end
 
 
-  private
-  def item_params
-    params.require(:item).permit(:name, :size, :condition, :shipping_method, :shipping_days, :prefecture_id, :shipping_price, :price, :text, :category_id, :brand_id, :seller_id, :buyer_id, :sale_status, item_images_attributes: [:image]).merge(seller_id: current_user.id)
-  end
+#   private
+#   def item_params
+#     params.require(:item).permit(:name, :size, :condition, :shipping_method, :shipping_days, :prefecture_id, :shipping_price, :price, :text, :category_id, :brand_id, :seller_id, :buyer_id, :sale_status, item_images_attributes: [:image]).merge(seller_id: current_user.id)
+#   end
 
-end
+# end
