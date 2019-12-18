bcab6793e4b71421d970e0cc674029b6b583e4de
kasugatakuya <rxsgw924@mineo.jp>
2019-12-18 20:02:29 +0900

    削除setting

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
 
-  before_action :set_item, only: [:show, :edit]
+  before_action :set_item, only: [:show, :edit, :destroy]
 
   def index
     @ladies = Item.where(category_id: "2")
@@ -33,11 +33,7 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item = Item.find(params[:id])
-    if item.seller_id == current_user.id
-      item.destroy
-      redirect_to root_path
-    end
+    redirect_to root_path if @item.seller_id == current_user.id && @item.destroy
   end
 
 
