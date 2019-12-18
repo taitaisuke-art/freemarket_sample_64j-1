87f5de82bff0f694c380d2a52d0b704cdffdee06
kasugatakuya <rxsgw924@mineo.jp>
2019-12-18 19:47:05 +0900

    商品削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -33,6 +33,11 @@ class ItemsController < ApplicationController
   end
 
   def destroy
+    item = Item.find(params[:id])
+    if item.seller_id == current_user.id
+      item.destroy
+      redirect_to root_path
+    end
   end
 
 
