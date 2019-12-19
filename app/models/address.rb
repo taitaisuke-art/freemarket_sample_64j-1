aa418098b7d8061619a97a923baf8d6e8baa00b0
pekepon476 <57280168+pekepon476@users.noreply.github.com>
2019-12-19 20:36:24 +0900

    検索機能

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,7 +1,6 @@
 class Address < ApplicationRecord
   belongs_to :user
   validates :first_name, :last_name, :first_name_kana, :post_cord, :city, :address, presence: true
-  validates :post_cord, length: {is: 8}
   
 
   # active_hashのアソシエーション
