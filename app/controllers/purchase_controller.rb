8a817ddd408137309bd771a42af7a2e184c18431
pekepon476 <57280168+pekepon476@users.noreply.github.com>
2019-12-20 14:36:09 +0900

    商品購入

--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -20,7 +20,7 @@ class PurchaseController < ApplicationController
   def pay
     Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
     Payjp::Charge.create(
-    :amount => 13500, #支払金額を入力（itemテーブル等に紐づけても良い）
+    :amount => @item.price, #支払金額を入力（itemテーブル等に紐づけても良い）
     :customer => @card.customer_id, #顧客ID
     :currency => 'jpy', #日本円
   )
