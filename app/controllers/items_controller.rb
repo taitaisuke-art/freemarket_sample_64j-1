d92ce7d799882337b7cced1e687804fea9201763
tikayk <tikayk@gmail.com>
2019-12-24 10:28:50 +0900

    sold out(商品詳細のみ)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -66,6 +66,7 @@ class ItemsController < ApplicationController
   end
 
 
+
   private
   def item_params
     params.require(:item).permit(:name, :size, :condition, :shipping_method, :shipping_days, :prefecture_id, :shipping_price, :price, :text, :category_id, :brand_id, :seller_id, :buyer_id, :sale_status, item_images_attributes: [:image]).merge(seller_id: current_user.id)
