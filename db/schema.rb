63535b250adced02677694fef3b1acde93fb332d
pekepon476 <57280168+pekepon476@users.noreply.github.com>
2019-12-26 14:29:27 +0900

    db

--- db/schema.rb
+++ db/schema.rb
@@ -70,6 +70,16 @@ ActiveRecord::Schema.define(version: 20191224110656) do
     t.index ["user_id"], name: "index_comments_on_user_id", using: :btree
   end
 
+  create_table "commnets", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.text     "text",       limit: 65535, null: false
+    t.integer  "item_id",                  null: false
+    t.integer  "user_id",                  null: false
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+    t.index ["item_id"], name: "index_commnets_on_item_id", using: :btree
+    t.index ["user_id"], name: "index_commnets_on_user_id", using: :btree
+  end
+
   create_table "genres", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",       null: false
     t.datetime "created_at", null: false
@@ -160,6 +170,8 @@ ActiveRecord::Schema.define(version: 20191224110656) do
   add_foreign_key "brand_genres", "genres"
   add_foreign_key "comments", "items"
   add_foreign_key "comments", "users"
+  add_foreign_key "commnets", "items"
+  add_foreign_key "commnets", "users"
   add_foreign_key "item_images", "items"
   add_foreign_key "items", "categories"
   add_foreign_key "likes", "items"
